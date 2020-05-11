9dd34a4a9d578e09fff3a4ff31fa377920adfd7c
dg022 <david.george.2480@gmail.com>
2020-05-10 21:36:06 -0600

    adding youtube api

--- src/comp/App.js
+++ src/comp/App.js
@@ -1,15 +1,33 @@
 import React from 'react'; 
 import SearchBar from './SearchBar'; 
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+    
+    search = async (term)=>{
+        //The SearchBar calls the search funciton with the term
+        // And here we will make the api request
+
+        const result = await youtube.get("/search", {
+            params: {
+              q: term,
+              part: "snippet",
+              maxResults: 5,
+              key: 'AIzaSyBSAzBSy4bhfG8JaCmptEDdreLpQXdAAbQ'            }
+          });
+
+
+        console.log(result);
+    }
+
 
 render(){
 
     return(
 
-        <div className="container">
+        <div className="ui container">
 
-            <SearchBar/>
+            <SearchBar Search={this.search} />
             
             
             </div>
--- src/comp/SearchBar.js
+++ src/comp/SearchBar.js
@@ -5,15 +5,31 @@ class SearchBar extends React.Component{
 
 state = {term: ''}; 
 
+ change = (event) =>{
+this.setState({term:event.target.value});
+};
+
+submit = (event) =>{
+event.preventDefault(); 
+this.props.Search(this.state.term);
+//this.props.Search.search(this.state.term);
+
+//To Do this term will be used to call the YouTube api
+
+};
+
 render(){
 
 return(
     <div className="search-bar ui segment">
-        <form className ="ui form">
+        <form onSubmit={this.submit}className ="ui form">
             <div className="field" >
 
                 <label>Video Search</label>
-                <input type="text"/>
+                <input 
+                onChange={this.change }
+                value={this.state.term}
+                type="text"/>
             </div>
         </form>
        
