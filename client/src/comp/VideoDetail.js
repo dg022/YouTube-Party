d290eb8f5d07cbc28abfa08e0fdfcd4dc799078f
dg022 <david.george.2480@gmail.com>
2020-05-15 16:29:30 -0600

    Adding YouTube Iframe Api

--- client/src/comp/VideoDetail.js
+++ client/src/comp/VideoDetail.js
@@ -1,37 +1,43 @@
 import React from 'react'; 
-
+import YouTube from 'react-youtube';
 // You need to pass a funciton to VideoDetail 
 // Which will then be called from inside, 
-
-const VideoDetail = ({video}) =>{
+//props.video.id.videoId ---> This  is the link to the video
+class VideoDetail extends React.Component{
   
-    if(video == null){
+    render() {
+
+        const opts = {
+          height: '390',
+          width: '640',
+          playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+          },
+        };
+
+        if(this.props.video==null){
+        return <div></div>; 
+        }
+
         return(
-            <div></div>
+
+            <div>
+                <div  className="ui embed">
+                <YouTube videoId={this.props.video.id.videoId} opts={opts} onReady={this._onReady} />;
+        
+                </div>
+        
+                <div className = "ui segment">
+                <h4 className="ui header">{this.props.video.snippet.title}</h4>
+                <p>{this.props.video.snippet.description}</p>
+                </div>
+        
+            </div>
+        
         );
-
-    }
+      }
     
-    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
-
-
-return(
-
-    <div>
-        <div onClick={() => console.log("HeSWAG") } className="ui embed">
-            <iframe id="myFrame" onClick={()=>console.log("MILGYA")} src={videoSrc} />
-
-        </div>
-
-        <div className = "ui segment">
-        <h4 className="ui header">{video.snippet.title}</h4>
-        <p>{video.snippet.description}</p>
-        </div>
-
-    </div>
-
-);
-
 }
 
 
