6d96f9755332256c90b99b28912bf0de3dd6dc54
dg022 <david.george.2480@gmail.com>
2020-05-24 18:52:07 -0600

    fixed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
-KEY: 'mongodb+srv://davidg022:Peterspider0136808251441!@cluster0-sveez.mongodb.net/test?retryWrites=true&w=majority'
+KEY: 'mongodb+srv://davidg022:@cluster0-sveez.mongodb.net/test?retryWrites=true&w=majority'
 
 }
