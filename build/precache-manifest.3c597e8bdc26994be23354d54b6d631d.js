79cdee5ba0eefd9b80a6c340c635df2aa98eacc0
dg022 <david.george.2480@gmail.com>
2020-05-26 02:37:16 -0600

    fixed

---
+++ build/precache-manifest.3c597e8bdc26994be23354d54b6d631d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f03952388892758a17f1d28dbf9e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc65a157801bbe9519f",
+    "url": "/static/css/main.cf3dd4f3.chunk.css"
+  },
+  {
+    "revision": "b152595d02465a76d168",
+    "url": "/static/js/2.843cd59f.chunk.js"
+  },
+  {
+    "revision": "d9dc00aa38d7f9cc3b31a0ee0b4901a5",
+    "url": "/static/js/2.843cd59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc65a157801bbe9519f",
+    "url": "/static/js/main.58b05a9f.chunk.js"
+  },
+  {
+    "revision": "037cba8b1e5aeebb1b7a",
+    "url": "/static/js/runtime-main.ef4f1f20.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-app.get('/*', function (req, res) {
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+
+app.listen(process.env.PORT || 8080);
