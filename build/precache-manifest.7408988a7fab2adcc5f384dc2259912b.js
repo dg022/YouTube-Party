968911203d1776cdc552fba1b3b1288a753b312b
dg022 <david.george.2480@gmail.com>
2020-06-15 03:21:45 -0600

    final dep

---
+++ build/precache-manifest.7408988a7fab2adcc5f384dc2259912b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e707e9bd1a59e612c3fa5f5fa01ca25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b70de0f280bcbf9cee",
+    "url": "/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "5b83682c56afcec1fd71",
+    "url": "/static/css/main.17e30b19.chunk.css"
+  },
+  {
+    "revision": "b4b70de0f280bcbf9cee",
+    "url": "/static/js/2.07a454c2.chunk.js"
+  },
+  {
+    "revision": "ff64921d3a7f20fd3bc941c71f83d3b4",
+    "url": "/static/js/2.07a454c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b83682c56afcec1fd71",
+    "url": "/static/js/main.9f17e746.chunk.js"
+  },
+  {
+    "revision": "037cba8b1e5aeebb1b7a",
+    "url": "/static/js/runtime-main.ef4f1f20.js"
+  }
+]);
